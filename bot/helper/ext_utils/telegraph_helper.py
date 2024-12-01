6089c46c8ee8360989b971b2b45c257b462abe1a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2024-12-01 13:52:17 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -3,7 +3,7 @@ from secrets import token_urlsafe
 from telegraph.aio import Telegraph
 from telegraph.exceptions import RetryAfterError
 
-from bot import LOGGER
+from tghbot import LOGGER
 
 
 class TelegraphHelper:
